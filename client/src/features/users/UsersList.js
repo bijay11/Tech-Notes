baf993169a505866d243a7a2762856c7cd17e668
Bijay Tamang <tmg.bijay11@gmail.com>
2022-12-18 21:49:35 -0500

    display users

--- client/src/features/users/UsersList.js
+++ client/src/features/users/UsersList.js
@@ -1,5 +1,45 @@
 import React from "react";
+import { useGetUsersQuery } from "./usersApiSlice";
+import { User } from "./User";
 
-export const UsersList = () => {
-  return <h1>UsersList</h1>;
+const UsersList = () => {
+  const {
+    data: users,
+    isLoading,
+    isSuccess,
+    isError,
+    error,
+  } = useGetUsersQuery();
+
+  if (isLoading) return <p>Loading...</p>;
+  if (isError) return <p className="errmsg">{error?.data?.message}</p>;
+
+  if (isSuccess) {
+    const { ids } = users;
+
+    const tableContent = ids?.length
+      ? ids.map((userId) => <User key={userId} userId={userId} />)
+      : null;
+
+    return (
+      <table className="table table--users">
+        <thead className="table__thead">
+          <tr>
+            <th scope="col" className="table_th user_username">
+              Username
+            </th>
+            <th scope="col" className="table_th user_roles">
+              Roles
+            </th>
+            <th scope="col" className="table_th user_edit">
+              Edit
+            </th>
+          </tr>
+        </thead>
+        <tbody>{tableContent}</tbody>
+      </table>
+    );
+  }
 };
+
+export { UsersList };
