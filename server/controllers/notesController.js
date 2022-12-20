168bbf4ce2d106b3e38715c4cd69433157bf4995
Bijay Tamang <tmg.bijay11@gmail.com>
2022-12-19 21:50:59 -0500

    changed boolean check to null or undefined

--- server/controllers/notesController.js
+++ server/controllers/notesController.js
@@ -52,7 +52,7 @@ const updateNote = asyncHandler(async (req, res) => {
   const { id, user, title, text, completed } = req.body;
 
   // Confirm data
-  if (!id || !user || !title || !text || !completed)
+  if (!id || !user || !title || !text || completed == undefined)
     return res.status(400).json({ message: "All fields are required." });
 
   // Confirm note exists to update
