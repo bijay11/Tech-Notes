b18092d46e42e0f60a56a0c36d57f0b42521eb01
Bijay Tamang <tmg.bijay11@gmail.com>
2023-02-11 09:54:07 -0500

    allow notes to delete by admin or manager only

--- client/src/features/notes/EditNoteForm.js
+++ client/src/features/notes/EditNoteForm.js
@@ -3,8 +3,10 @@ import { useUpdateNoteMutation, useDeleteNoteMutation } from "./notesApiSlice";
 import { useNavigate } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSave, faTrashCan } from "@fortawesome/free-solid-svg-icons";
+import { useAuth } from "./../../hooks/useAuth";
 
 export const EditNoteForm = ({ note, users }) => {
+  const { isManager, isAdmin } = useAuth();
   const [updateNote, { isLoading, isSuccess, isError, error }] =
     useUpdateNoteMutation();
   const [
@@ -70,6 +72,8 @@ export const EditNoteForm = ({ note, users }) => {
 
   const errContent = (error?.data?.message || delError?.data?.message) ?? "";
 
+  const isUserAdminOrManager = isManager || isAdmin;
+
   return (
     <>
       {errContent && <p className={errClass}>{errContent}</p>}
@@ -85,13 +89,15 @@ export const EditNoteForm = ({ note, users }) => {
             >
               <FontAwesomeIcon icon={faSave} />
             </button>
-            <button
-              className="icon-button"
-              title="Delete"
-              onClick={onDeleteNoteClicked}
-            >
-              <FontAwesomeIcon icon={faTrashCan} />
-            </button>
+            {isUserAdminOrManager && (
+              <button
+                className="icon-button"
+                title="Delete"
+                onClick={onDeleteNoteClicked}
+              >
+                <FontAwesomeIcon icon={faTrashCan} />
+              </button>
+            )}
           </div>
         </div>
         <label className="form__label" htmlFor="note-title">
